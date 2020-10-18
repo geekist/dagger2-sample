294ffda460edb802ad7400d1e7ed25c46c11a9d3
geekist <geekist@outlook.com>
2020-10-18 16:23:41 +0800

    finish qialifier and named annotation

--- Dagger2Sample/app/src/main/java/com/jon/dagger2sample/bean/FruitModule.java
+++ Dagger2Sample/app/src/main/java/com/jon/dagger2sample/bean/FruitModule.java
@@ -1,12 +1,23 @@
 package com.jon.dagger2sample.bean;
 
+import javax.inject.Named;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class FruitModule {
     @Provides
-    public Fruit provideFruit() {
+    //@Named("Apple")
+    @AppleFruit
+    public Fruit provideApple() {
         return new Apple();
+    }
+
+    @Provides
+    //@Named("Banana")
+    @BananaFruit
+    public Fruit provideBanana() {
+        return new Banana();
     }
 }
--- Dagger2Sample/app/src/main/java/com/jon/dagger2sample/ui/MainActivity.java
+++ Dagger2Sample/app/src/main/java/com/jon/dagger2sample/ui/MainActivity.java
@@ -5,31 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.jon.dagger2sample.R;
-import com.jon.dagger2sample.bean.Car;
+import com.jon.dagger2sample.bean.AppleFruit;
+import com.jon.dagger2sample.bean.BananaFruit;
 import com.jon.dagger2sample.bean.DaggerActivityComponent;
 import com.jon.dagger2sample.bean.Fruit;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 public class MainActivity extends AppCompatActivity {
 
     @Inject
-    Car car;
+    @AppleFruit
+    // @Named("Apple")
+    public Fruit fruitApple;
+
 
     @Inject
-    Fruit fruit;
+    @BananaFruit
+    //@Named("Banana")
+    public Fruit fruitBanana;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       DaggerActivityComponent.builder().build().inject(this);
-       car.getName();
+        DaggerActivityComponent.builder().build().inject(this);
 
-       String str = fruit.toString();
-
-
+        String strApple = fruitApple.toString();
+        String strBanana = fruitBanana.toString();
 
     }
 }
